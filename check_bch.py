44cd2ffd565868767c73bf999b992343fecf285c
jimz85 <25851759@qq.com>
2026-04-26 12:09:29 +0800

    cleanup: 删除v1-v3研究废弃文件(-5589行)

--- check_bch.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""BCH数据检查"""
-import pandas as pd
-
-path = '/Users/jimingzhang/Desktop/crypto_data_Pre5m/BCH_USDT_5m_from_20180101.csv'
-df = pd.read_csv(path, nrows=10)
-df.columns = [c.lstrip('\ufeff') for c in df.columns]
-print(df.columns.tolist())
-print(df.head())
